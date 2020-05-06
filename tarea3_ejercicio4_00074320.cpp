e3379c612cbb455c4d9af6cac1eecd0b136ff512
barhedd <00074320@uca.edu.sv>
2020-05-06 10:10:06 -0600

    ejercicio 4

---
+++ tarea3_ejercicio4_00074320.cpp
@@ -0,0 +1,19 @@
+/*  Realizar un programa en c++ que reciba 1 producto comprado (nombre,
+precio y cantidad comprada) y mostrar el total de dinero gastado. */
+#include <iostream>
+using namespace std;
+int main(){
+    string producto;
+    float precio, total;
+    int cantidad;
+    cout<<"**** PROGRAMA QUE CALCULA EL TOTAL DE DINERO GASTADO EN UN PRODUCTO ****"<<endl;
+    cout<<"Ingrese el nombre del producto: ";
+    cin>>producto;
+    cout<<"Ingrese el precio del producto: $";
+    cin>>precio;
+    cout<<"Ingrese la cantidad de unidades compradas: ";
+    cin>>cantidad;
+    total=precio*cantidad;
+    cout<<"El total de dinero gastado en la compra del producto "<<producto<<" es: $"<<total;
+    return 0;
+}
