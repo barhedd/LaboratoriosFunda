dc640283f62a641ca916cfda4d026ee5b7126f0e
barhedd <00074320@uca.edu.sv>
2020-05-06 10:08:48 -0600

    ejercicio 1

---
+++ tarea3_ejercicio1_00074320.cpp
@@ -0,0 +1,13 @@
+/* Realizar un programa en c++ que devuelva el promedio de 3 números
+enteros ingresados por el usuario. */
+#include <iostream>
+using namespace std;
+int main(){
+    int promedio, num1, num2, num3;
+    cout<<"Hola, bienvenido a mi programa que te ayudará a calcular el promedio de 3 números enteros"<<endl;
+    cout<<"Por favor ingresa los 3 números: "<<endl;
+    cin>>num1>>num2>>num3;
+    promedio=(num1+num2+num3)/3;
+    cout<<"El promedio de los números es: "<<promedio;
+    return 0;
+}
