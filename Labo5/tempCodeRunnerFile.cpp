c53241286e19872dc9174e72353af6d9e93b8af9
barhedd <00074320@uca.edu.sv>
2020-06-14 13:28:06 -0600

    correcciones

--- Labo5/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
- cout<<"\n********************************************************";
-    cout<<"\n    PROGRAMA QUE MUESTRA LA HORA ACTUAL DEL SISTEMA"<<endl;
-    cout<<"\t\tY UN SEGUNDO DESPUES"<<endl;
-    cout<<"********************************************************\n\n";
